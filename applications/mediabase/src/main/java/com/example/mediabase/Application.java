0aecfd1985ceb189337205847e05a2e597c08788
Mujtaba Aslam <mujtaba.aslam@perficient.com>
2019-09-20 11:35:09 -0400

    Setting up project

---
+++ applications/mediabase/src/main/java/com/example/mediabase/Application.java
@@ -0,0 +1,20 @@
+package com.example.mediabase;
+
+import com.example.mediabase.movies.ActionServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean registerActionServlet(ActionServlet actionServlet) {
+        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+    }
+}
