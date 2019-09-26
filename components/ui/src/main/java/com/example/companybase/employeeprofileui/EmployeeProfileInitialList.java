2a0b3963f30b516fa9db840ac9785e136ff77ef9
Mujtaba Aslam <mujtaba.aslam@perficient.com>
2019-09-26 09:30:39 -0400

    fixed issue with repeated set ups

--- components/ui/src/main/java/com/example/companybase/employeeprofileui/EmployeeProfileInitialList.java
+++ components/ui/src/main/java/com/example/companybase/employeeprofileui/EmployeeProfileInitialList.java
@@ -10,8 +10,19 @@ public class EmployeeProfileInitialList {
 
     public List<EmployeeProfileUI> asList(List<Long> ids){
         List<EmployeeProfileUI> list = new ArrayList<>();
-        list.add(new EmployeeProfileUI(ids.get(0), "a", "b", "c"));
-        list.add(new EmployeeProfileUI(ids.get(1), "d", "ee", "f"));
+        int repeat = ids.size() / 10;
+        for(int i = 0; i < repeat; i++) {
+            list.add(new EmployeeProfileUI(ids.get(0 + (10 * i)), "Detroit, MI", "Java, Spring, Cloud Foundry", "Cloud Foundry"));
+            list.add(new EmployeeProfileUI(ids.get(1 + (10 * i)), "Livonia, MI", "Java, C, Spring", "None"));
+            list.add(new EmployeeProfileUI(ids.get(2 + (10 * i)), "Chicago, IL", "Agile, Microsoft Office", "None"));
+            list.add(new EmployeeProfileUI(ids.get(3 + (10 * i)), "New York, NY", "Salesforce, Microsoft Excel", "Salesforce"));
+            list.add(new EmployeeProfileUI(ids.get(4 + (10 * i)), "San Francisco, CA", "Management", "None"));
+            list.add(new EmployeeProfileUI(ids.get(5 + (10 * i)), "Boston, MA", "Networking", "None"));
+            list.add(new EmployeeProfileUI(ids.get(6 + (10 * i)), "Livonia, MI", "Java", "AWS"));
+            list.add(new EmployeeProfileUI(ids.get(7 + (10 * i)), "San Diego, CA", "Java, C, Spring, Cloud Foundry", "Cloud Foundry"));
+            list.add(new EmployeeProfileUI(ids.get(8 + (10 * i)), "Jersey City, NJ", "Team Management, Java, Spring", "None"));
+            list.add(new EmployeeProfileUI(ids.get(9 + (10 * i)), "Livonia, MI", "Communication", "None"));
+        }
         return list;
     }
 }
